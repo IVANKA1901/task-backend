389893e3fef1e1e23f5e328fa9903e66471f9ba2
IVANKA1901 <125952478+IVANKA1901@users.noreply.github.com>
2023-09-04 17:44:36 +0200

    add login/ logout

--- routes/auth-router.js
+++ routes/auth-router.js
@@ -3,6 +3,7 @@ import express from "express";
 import { validateBody } from "../decorators/index.js";
 import usersSchemas from "../Schemas/users-schemas.js";
 import authControllers from "../controllers/auth-controllers.js";
+import authenticate from "../middlewares/authenticate.js";
 
 const authRouter = express.Router();
 
@@ -10,6 +11,20 @@ authRouter.post(
   "/signup",
   validateBody(usersSchemas.userSignupSchema),
   authControllers.signup
+);
+
+authRouter.post(
+  "/login",
+  validateBody(usersSchemas.userLoginSchema),
+  authControllers.login
+);
+
+authRouter.post("/logout", authenticate, authControllers.logout);
+
+authRouter.post(
+  "/forgot-password",
+  validateBody(usersSchemas.userEmailSchema),
+  authControllers.resendPassword
 );
 
 export default authRouter;
