cdc97297bfb609119013c7f78044e5a95931d789
IVANKA1901 <125952478+IVANKA1901@users.noreply.github.com>
2023-09-04 16:55:30 +0200

    add user schema

---
+++ Schemas/users-schemas.js
@@ -0,0 +1,44 @@
+import Joi from "joi";
+import { joiPasswordExtendCore } from "joi-password";
+const joiPassword = Joi.extend(joiPasswordExtendCore);
+
+const userSignupSchema = Joi.object({
+  password: joiPassword
+    .string()
+    .minOfLowercase(1)
+    .minOfUppercase(1)
+    .minOfNumeric(4)
+    .noWhiteSpaces()
+    .onlyLatinCharacters()
+    .messages({
+      "password.minOfUppercase":
+        "{#label} should contain at least {#min} uppercase character",
+      "password.minOfSpecialCharacters":
+        "{#label} should contain at least {#min} special character",
+      "password.minOfLowercase":
+        "{#label} should contain at least {#min} lowercase character",
+      "password.minOfNumeric":
+        "{#label} should contain at least {#min} numeric character",
+      "password.noWhiteSpaces": "{#label} should not contain white spaces",
+      "password.onlyLatinCharacters":
+        "{#label} should contain only latin characters",
+    }),
+  email: Joi.string()
+    .pattern(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)
+    .required()
+    .trim(),
+  name: Joi.string().min(2).required().trim(),
+});
+
+const userLoginSchema = Joi.object({
+  email: Joi.string()
+    .pattern(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)
+    .required()
+    .trim(),
+  password: Joi.string().min(6).max(16).required(),
+});
+
+export default {
+  userSignupSchema,
+  userLoginSchema,
+};
